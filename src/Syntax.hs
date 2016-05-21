65f004d1c84afe368e54f39b56ee81980275885a
juanbono <juanbono94@gmail.com>
2016-05-21 17:17:01 -0300

    los tipos de datos numericos y los LispVal los pase a GADTs, capaz sirva de algo, queda pendiente hacer la instancia de functor para los tipos numericos, asi me ahorro codigo en varias partes

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,8 +1,8 @@
+{-# LANGUAGE GADTs #-}
 module Syntax where
-
 import Data.Complex (Complex (..))
 
-instance Show LispVal where
+instance Show (LispVal a) where
   show (Atom s) =  s
   show (List xs) =  "(" ++ unwordsList xs ++ ")"
   show (DottedList xs x) = "(" ++ unwordsList xs ++ " . " ++ show x ++ ")"
@@ -12,32 +12,39 @@ instance Show LispVal where
   show (Bool False) = "#f"
   show (Character c) = [c]
 
-data LispVal = Atom String
-             | List [LispVal]
-             | DottedList [LispVal] LispVal
-             | Number NumberType
-             | String String
-             | Bool Bool
-             | Character Char
+data LispVal a where
+  Atom       :: String -> LispVal String
+  List       :: [LispVal a] -> LispVal [a]
+  DottedList :: [LispVal a] -> LispVal a -> LispVal ([LispVal a], LispVal a)
+  Number     :: NumberType a -> LispVal a
+  String     :: String -> LispVal String
+  Bool       :: Bool -> LispVal Bool
+  Character  :: Char -> LispVal Char
 
-data NumberType = Integer Int
-                | Float Double
-                | Complex (Complex Double)
-                | Rational Rational
-  deriving Show -- hacer la instancia a mano
+data NumberType a where
+  Float    :: Double -> NumberType Double
+  Complex  :: Complex Double -> NumberType (Complex Double)
+  Rational :: Rational -> NumberType Rational
+  Integer  :: Int -> NumberType Int
 
-unwordsList :: [LispVal] -> String
+instance Show (NumberType a) where
+  show (Float x) = show x
+  show (Complex x) = show x
+  show (Rational x) = show x
+  show (Integer x) = show x
+
+unwordsList :: [LispVal a] -> String
 unwordsList = unwords . map show
 
-toFloat :: Double -> LispVal
+toFloat :: Double -> LispVal Double
 toFloat = Number . Float
 
-toRational :: Rational -> LispVal
+toRational :: Rational -> LispVal Rational
 toRational = Number . Rational
 
-toComplex :: Complex Double -> LispVal
+toComplex :: Complex Double -> LispVal (Complex Double)
 toComplex = Number . Complex
 
-toInt :: Int -> LispVal
+toInt :: Int -> LispVal Int
 toInt = Number . Integer
 
