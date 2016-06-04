c41671b5ed15e67cf507f3ff27412e1543757537
juanbono <juanbono94@gmail.com>
2016-06-04 17:20:41 -0300

    agregado codigo del capitulo de manejo de errores

--- src/Eval.hs
+++ src/Eval.hs
@@ -2,6 +2,9 @@ module Eval (eval) where
 
 import Syntax
 import SimpleParser
+import Text.ParserCombinators.Parsec
+import Control.Monad.Error
+
 
 eval :: LispVal -> LispVal
 eval val@(String _) = val
@@ -32,3 +35,35 @@ unpackNum (String n) = let parsed = reads n :: [(Int, String)] in
                               else fst $ head parsed
 unpackNum (List [n]) = unpackNum n
 unpackNum _ = 0
+
+-- capaz esto tengo que moverlo a otro fichero u organizar mejor
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
+
+instance Show LispError where
+  show (NumArgs expected found) = "Expected " ++ show expected ++ " args; found values " ++ unwordsList found
+  show (TypeMismatch expected found) = "Invalid type: expected " ++ expected ++ ", found " ++ show found
+  show (Parser parseErr) = "Parse error at " ++ show parseErr
+  show (BadSpecialForm message form) = message ++ ": " ++ show form
+  show (NotFunction message func) = message ++ ": " ++ show func
+  show (UnboundVar message varname) = message ++ ": " ++ varname
+  show (Default message) = message
+
+instance Error LispError where
+  noMsg = Default "An error has ocurred"
+  strMsg = Default
+
+type ThrowsError = Either LispError
+
+trapError action = catchError action (return . show)
+
+extractValue :: ThrowsError a -> a
+extractValue (Right val) = val
+
